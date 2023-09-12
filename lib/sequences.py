66304b09f0241a6cee8d75e663911980c304860a
Kim Love <kimberlymlove15@gmail.com>
2023-09-12 16:36:56 -0400

    All tests passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,12 @@
 
 def print_fibonacci(length):
     pass
+    if length == 0:
+        print([])
+    else:
+        fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,144,233,377,610,987]
+        
+        newfib= list()
+        for i in range(length):
+            newfib.append(fib[i])
+        print(newfib)
